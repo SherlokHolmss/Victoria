2812d18c626c117c2503c80a97b6dee527e73925
Miha77777 <lv11miha@gmail.com>
2025-02-10 23:25:27 +0200

    added modal

---
+++ js/modal.js
@@ -0,0 +1,24 @@
+const refs = {
+    openModalBtns: document.querySelectorAll(".modal-open"),
+    closeModalBtn: document.querySelector(".modal-close"),
+    backdrop: document.querySelector(".backdrop"),
+    modal: document.querySelector(".modal"),
+    body: document.querySelector("body"),
+};
+
+function toggleModal() {
+    refs.backdrop.classList.toggle("is-hidden");
+    refs.body.classList.toggle("modal-on");
+}
+
+refs.openModalBtns.forEach(el => {
+    el.addEventListener("click", toggleModal);
+});
+
+refs.closeModalBtn.addEventListener("click", toggleModal);
+
+refs.backdrop.addEventListener("click", ev => {
+    if (ev.target.classList.contains("backdrop")) {
+        toggleModal();
+    }
+});
