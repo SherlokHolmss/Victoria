b5227d5d3e05101740b7dc3a1483b65f5decc808
Davud <david.poninka@gmail.com>
2025-02-11 20:07:11 +0200

    push

---
+++ js/gallery.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const slider = document.querySelector(".gallery-list");
+    const prevBtn = document.querySelector(".gallery-prev");
+    const nextBtn = document.querySelector(".gallery-next");
+    const step = 325; 
+    prevBtn.addEventListener("click", function () {
+        slider.scrollBy({ left: -step, behavior: "smooth" });
+    });
+
+    nextBtn.addEventListener("click", function () {
+        slider.scrollBy({ left: step, behavior: "smooth" });
+    });
+});
