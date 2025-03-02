09087922529334c9279e26f3dc5eaaeb7b7b6ba0
Davud <david.poninka@gmail.com>
2025-03-02 14:40:00 +0200

    push

--- js/gallery.js
+++ js/gallery.js
@@ -1,14 +1,21 @@
+const slider = document.querySelector(".splide");
 
-document.addEventListener("DOMContentLoaded", function () {
-    const slider = document.querySelector(".gallery-list");
-    const prevBtn = document.querySelector(".gallery-prev");
-    const nextBtn = document.querySelector(".gallery-next");
-    const step = 325; 
-    prevBtn.addEventListener("click", function () {
-        slider.scrollBy({ left: -step, behavior: "smooth" });
-    });
-
-    nextBtn.addEventListener("click", function () {
-        slider.scrollBy({ left: step, behavior: "smooth" });
-    });
+let splide = new Splide(slider, {
+    type: 'loop',
+    perPage: 3,
+    focus: 'center',
+    start  : 3,
+    gap: "20px",
+    breakpoints: {
+        1024: {
+            perPage: 2,
+            gap: '15px'
+        },
+        768: {
+            perPage: 1, 
+            gap: '10px'
+        }
+    }
 });
+splide.mount();
+
